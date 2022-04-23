1e54a7057f4308f1f580b0e7888ca822c9c69d8a
Wee Seng <wee.bscl@gmail.com>
2022-04-23 14:15:32 +0800

    edit settings

--- elitelibFlask/config/settings.py
+++ elitelibFlask/config/settings.py
@@ -1,6 +1,5 @@
-#centralised settings as diff service providers will have diff database and keys
-
 class Settings:
+    secretKey = 'What1The2Duck3Bar4be5que69'
     host = 'elitelib22.mysql.pythonanywhere-services.com'
     database = 'elitelib22$libcatalogue'
     user = 'elitelib22'
