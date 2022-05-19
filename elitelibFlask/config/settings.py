e7372206ec499d0a2ec935b3f182de9b0f9d87e5
Wee Seng <75309293+Akimori-236@users.noreply.github.com>
2022-05-19 23:11:43 +0800

    Delete settings.py
    delete settings

--- elitelibFlask/config/settings.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = 'xnjkc6v46wIiJ6WonlXAYA'
-
-class Settings:
-    SECRET_KEY = 'xnjkc6v46wIiJ6WonlXAYA'
-    host = 'elitelib22.mysql.pythonanywhere-services.com'
-    database = 'elitelib22$libcatalogue'
-    user = 'elitelib22'
-    password = 'librarydb'
